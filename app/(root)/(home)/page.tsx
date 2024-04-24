d7887d8cc8a553e4ce858851e8ec9c2f6cfa28e6
filip <filip.sostaric@protonmail.com>
2024-04-24 22:41:06 +0200

    update

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { UserButton } from '@clerk/nextjs';
+
+const Home = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
